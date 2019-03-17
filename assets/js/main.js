76b2846aa6d02eb1fe32d18bd8a3132577bff772
F1rman <ihorkharchyshyn@gmail.com>
2019-03-17 11:51:59 +0200

    Logo ng-hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -278,56 +278,36 @@ app.controller('cardCtrl', function($scope, $rootScope) {
 app.controller('homeCtrl', function($scope, $rootScope, $location) {
   $rootScope.less_preview = [0, 1];
   var path = $location.path();
-
-  if (path == '/home') {
-    $('.home_lessons_wrapper .boxscroll').niceScroll('.wrap');
-    // if ($scope.selected == undefined) {
-    //   $scope.selected = 0;
-    // }
-    // $scope.select = function(index) {
-    //   $scope.selected = index;
-    //   $('.home_lessons_wrapper .boxscroll').getNiceScroll(0).doScrollLeft($('.home_lessons')[index].offsetLeft);
-    // };
-    // $('.boxscroll').scroll(function() {
-    //   var scrolled_x = $('.wrap').css('transform').slice(20, -4);
-    //   scrolled_x = Math.ceil(Number(scrolled_x));
-    //   if (scrolled_x == 0) {
-    //     scrolled_x = 0;
-    //   }
-    //   var pos_first = $('.home_lessons')[0].offsetLeft
-    //   pos_first = Math.ceil(Number(pos_first));
-    //   $scope.scrolled = (scrolled_x - pos_first) / 360;
-    //   $scope.$apply(() => {
-    //     $scope.scrolled = Math.ceil($scope.scrolled)
-    //
-    //   })
-    // })
-  }
-  if (path == '/lessons') {
-    $('.list_wrapper .lessons_title_scroll').niceScroll('.wrap');
-    if ($scope.selected1 == undefined) {
-      $scope.selected1 = 0;
-    }
-    $scope.select1 = function(index) {
-      $scope.selected1 = index;
-      $('.lessons_title_scroll').getNiceScroll(0).doScrollLeft($('.lesson_container')[index].offsetLeft);
-    };
-    $('.lessons_title_scroll').scroll(function() {
-      var scrolled_x = $('.less_mazafaka').css('transform').slice(20, -4);
-      scrolled_x = Math.ceil(Number(scrolled_x));
-      if (scrolled_x == 0) {
-        scrolled_x = 0;
-      }
-      var pos_first = $('.less_mazafaka')[0].offsetLeft
-      pos_first = Math.ceil(Number(pos_first));
-      $scope.scrolled1 = (scrolled_x - pos_first) / 360;
-      $scope.$apply(() => {
-        $scope.scrolled1 = Math.ceil($scope.scrolled1)
-      })
-
-    })
-
-  }
+  var h_wrapp = '.scrollbox';
+  $(h_wrapp).niceScroll('.wrap');
+//   if ($scope.selected == undefined) {
+//     $scope.selected = 0;
+//   }
+//   $scope.select = function(index) {
+//     $scope.selected = index;
+//     $(h_wrapp).getNiceScroll(0).doScrollLeft($('.scroll_inset')[index].offsetLeft);
+//   };
+//   $(h_wrapp).scroll(function() {
+//     var scrolled_x = $('.wrap').css('transform').slice(20, -4);
+//     scrolled_x = Math.ceil(Number(scrolled_x));
+//     if (scrolled_x == 0) {
+//       scrolled_x = 0;
+//     }
+//     var pos_first = $('.scroll_inset')[0].offsetLeft
+//     pos_first = Math.ceil(Number(pos_first));
+//     $scope.$apply(()=>{
+//       $scope.scrolled = Math.ceil((scrolled_x - pos_first) / 360);
+//       if (path == '/lessons') {
+//         $scope.$watch('scrolled',()=>{
+//           if ($scope.scrolled == 2) {
+//           $rootScope.app.alerts.goProPopup = true;
+//           $scope.scrolled = 1;
+//           $('.lessons_title_scroll').getNiceScroll(0).doScrollLeft(0);
+//           }
+//         })
+//       }
+//     });
+// });
 });
 app.controller('infoCtrl', function($scope, $rootScope) {});
 app.controller('proCtrl', function($scope, $rootScope) {});

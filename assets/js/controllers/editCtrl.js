f5171d0a93c2af1101f224c083a798cba605e2de
F1rman <ihorkharchyshyn@gmail.com>
2019-03-20 20:01:43 +0200

    Some fixes

--- assets/js/controllers/editCtrl.js
+++ assets/js/controllers/editCtrl.js
@@ -1,53 +1,78 @@
 app.controller('editCtrl', function($scope, $rootScope) {
   $scope.selected_option = $rootScope.app.filters[0];
-
   let selected_option;
   $scope.ta_maxLength = 400;
   $scope.ta_minLength = 40;
-
   $scope.selected = function(a) {
       $scope.selected_a = a;
-
     if (a == 1 || a == 3 || a == 4 ) {
+      // то шо не треба вводити в текстарію
       $scope.no = true;
       $scope.ta_maxLength = 0;
       $scope.ta_minLength = 0;
-    } else {
+    }
+     else {
+       if ($scope.newTask.textarea.length < $scope.ta_minLength) {
+         $scope.active_btn = false;
+         $scope.$apply();
+       }
       $scope.no = false;
       $scope.ta_maxLength = 400;
       $scope.ta_minLength = 40;
     }
-    console.log($scope.selected_a , a == 2 && $rootScope.data.user.isMember);
-    if (a == 2 && !$rootScope.data.user.isMember) {
-      $scope.no = true;
-      $scope.ta_maxLength = 0;
-      $scope.ta_minLength = 0;
-
-    }
-    console.log($rootScope.data.user.isMember,'$rootScope.data.user.isMember');
-
-  if (a > 1 && !$rootScope.data.user.isMember) {
-    $scope.no = true;
-  }
+  // Якщо ти не підписаний вибрав не хештег
     if (a > 0 && !$rootScope.data.user.isMember) {
+      $scope.active_btn = false; // якшо не підписаний and chose not hash bnt is not active
+      $scope.no = true;
       $rootScope.app.alerts.showError2 = !0;
+      $('.task_filter textarea').prop( "disabled", true );
       return;
     } else {
       $rootScope.app.alerts.showError2 = !1;
+      $('.task_filter textarea').prop( "disabled", false );
     }
+      if (a == 2 && !$rootScope.data.user.isMember) {
+          $scope.active_btn = false;
+          $scope.$apply();
+      }
+      else {
+        $scope.active_btn = true;
+      }
+    // ЯКЩО вибрав за фоловерами і ти підписаний або ні
     if (a > 2 && !!$rootScope.data.user.isMember) {
+      $scope.active_btn = false;
+        $('.task_filter textarea').prop( "disabled", true );
       $rootScope.app.alerts.showError3 = !0;
       return;
     }
      else {
       $rootScope.app.alerts.showError3 = !1;
+      $('.task_filter textarea').prop( "disabled", false );
     }
     $rootScope.newTask.type = $rootScope.app.filters[a][0];
     $scope.selected_option = $rootScope.app.filters[a];
     // $scope.$apply();
   }
+
+  // Якшо вводиш симовли в текстарію
+  $rootScope.$watch('newTask.textarea.length',()=>{
+
+    $scope.textarea_length = $rootScope.newTask.textarea.length;
+    if ($rootScope.newTask.textarea.length >= $scope.ta_minLength && $scope.selected_a != 2) {
+      $scope.active_btn = true;
+    }
+    else if ($rootScope.newTask.textarea.length >= $scope.ta_minLength && $scope.selected_a == 2 && $rootScope.data.user.isMember) {
+$scope.active_btn = true;
+    }
+    else {
+      $scope.active_btn = false;
+    }
+  })
   $rootScope.saveTask = function() {
-    if ($rootScope.newTask.textarea.length < $scope.ta_minLength || $scope.selected_a == 3 || $scope.selected_a == 4) {
+    if ($rootScope.newTask.textarea.length == undefined) {
+      $rootScope.newTask.textarea.length = 0;
+    }
+    if ($rootScope.newTask.textarea.length < $scope.ta_minLength || $scope.selected_a == 3 || $scope.selected_a == 4 || $scope.selected_a > 0 && !$rootScope.data.user.isMember) {
       return false;
     }
     if ($rootScope.app.taskFunc == 'add') {
